1faa26b25fda9b05fb3520232b36341f39f92f5b
Niraj Dodal <nirajdodal@Nirajs-MacBook-Air.local>
2021-04-22 16:45:29 +0530

    add appspec to check on code pipeline on ec2

---
+++ scripts/application_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#navigate to the working directory where we have our code
+cd /home/ec2-user/express-app
+
+#Close and reopen your terminal to start using nvm or run the following to use it now:
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#install node modules
+npm install
+
+#start node app in the background
+node index.js > app.out.log 2> app.err.log < /dev/null &
---
+++ scripts/application_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#stop existing node server
+echo "Stopping existing node server"
+pkill node
